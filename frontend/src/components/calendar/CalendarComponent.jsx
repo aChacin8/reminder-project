90c8f6f45ef5a26278b933fe2f9a26694d8f7960
mrMcAko <alechacin08@gmail.com>
2025-05-08 22:35:00 -0600

    Implementamos logica de coneion del backend en el frontend para visualizar los eventos creados.

--- frontend/src/components/calendar/CalendarComponent.jsx
+++ frontend/src/components/calendar/CalendarComponent.jsx
@@ -3,17 +3,43 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction'
+import { useEffect } from 'react';
+import { useEventContext } from '@/hooks/useEventContext';
+import { useAuthContext } from '@/hooks/useAuthContext'; // Importa el hook useAuth para obtener el token de autenticación
 import '@/styles/Calendar.scss';
-import { useContext } from 'react';
-import { EventContext } from '@/context/EventContext';
+
+
+const API_URL = import.meta.env.VITE_API_URL; // URL de la API desde el archivo .env
 
 const CalendarComponent = () => {
+    const {events, setEvents} = useEventContext(); //Obtenemos el contexto de los eventos
+    const { token } = useAuthContext(); // Obtiene el token de autenticación usando el hook useAuth
 
-    const {events, loading} = useContext(EventContext);
+
+    useEffect(()=> {
+        const getEvents = async () => {
+            try {
+                const response = await fetch(`${API_URL}/taskly/events`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}` //Para verificar el JWT
+                    }
+                })
+
+                const result = await response.json();
+                console.log("Eventos obtenidos:", result); //Muestra los eventos obtenidos en la consola
+                setEvents(result); //Actualiza el contexto de los eventos
+            } catch (error) {
+                console.log("Error al obtener eventos", error);
+            }
+        }
+        if (token) {
+            getEvents(); //Llama a la funcion para obtener los eventos
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [token]); //Se ejecuta cada vez que cambia el evento
     
-    if (loading){
-        return <p>Cargando eventos...</p>
-    }
 
     return (
         <div className='calendar'>
@@ -25,18 +51,22 @@ const CalendarComponent = () => {
                     center: 'title', //El mes en el que nos encontramos dentro del calendario
                     end: 'dayGridMonth,timeGridWeek,timeGridDay' //Se asignan los botones de visualizacion, por "Mes","Semana" y "Dia"
                 }}
-                events={events.map(event=>(
-                    {
-                        title: event.name,
-                        description: event.description,
-                        start: event.start_date,
-                        end: event.end_date,
+                events={events.map(event => {
+                    const start = new Date(event.event_start_date);
+                    const end = new Date(event.event_end_date);
+                
+                    return {
+                        title: event.event_name,
+                        description: event.event_description,
+                        start: isNaN(start.getTime()) ? null : start.toISOString(),
+                        end: isNaN(end.getTime()) ? null : end.toISOString(),
                         color: event.color
-                    }
-                ))}
+                    };
+                })}
             />
         </div>
     );
 };
 
 export default CalendarComponent;
+
