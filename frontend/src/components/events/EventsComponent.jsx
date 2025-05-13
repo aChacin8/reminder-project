13f248fa4b277f66ca929031bfd23ba267299a82
mrMcAko <alechacin08@gmail.com>
2025-05-13 14:12:25 -0600

    Agregamos las referencias de los componentes de actualizar evento y eliminarlo, para asi manejarlo de manera externa mejorando la estructura del componente.
    Se agregó la funcion de "handleEdit" que permite abrir el Modal para actualizar el evento

--- frontend/src/components/events/EventsComponent.jsx
+++ frontend/src/components/events/EventsComponent.jsx
@@ -3,12 +3,14 @@ import { useEffect } from 'react';
 import { useEventContext } from '@/hooks/useEventContext';
 import { useAuthContext } from '@/hooks/useAuthContext';
 import '@/styles/Calendar.scss';
-
+import UpdateEventsComponent from './UpdateEventsComponent';
+import DeleteEventsComponent from './DeleteEventsComponent';
 const API_URL = import.meta.env.VITE_API_URL; // URL de la API desde el archivo .env
 
 const EventsComponent = () => {
-    const { events, setEvents } = useEventContext();
+    const { events, setEvents, setSelectedEvent, setShowModal} = useEventContext();
     const { token } = useAuthContext();
+
 
     useEffect(() => {
         const fetchEvents = async () => {
@@ -27,69 +29,60 @@ const EventsComponent = () => {
             }
         };
         fetchEvents();
-
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [setEvents]); // Se asegura de que el efecto se ejecute solo una vez al montar el componente  
 
-    const updateEvents = async (data) => {
-        try {
-                
-                const response = await fetch(`${API_URL}/taskly/events/${data.id_event}`, {
-                    method: 'PATCH',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: `Bearer ${token}`
-                    },
-                    body: JSON.stringify(data)
-                });
-                const result = await response.json();
-                setEvents(result);
-            } catch (error) {
-                console.error('Error actualizar eventos:', error);
-            }
-        };
+        const handleEdit = (event) => {
+        setSelectedEvent(event);
+        setShowModal(true);
+        }
 
     return (
-        <Card className='eventCard'>
-            <Card.Body>
-                <Card.Title className='eventCard__title'>Mis Eventos</Card.Title>
-                <Card.Text className='eventCard__text'>
-                    Aquí puedes ver todos tus eventos programados.
-                </Card.Text>
-                <ul className='eventCard__list'>
-                    {Array.isArray(events) && events.length > 0 ? (
-                        events.map((event) => (
-                            <li key={event.id_events} className='mb-3' style={{ listStyle: 'none' }}>
-                                <Card>
-                                    <Card.Body className='eventCard__body'> 
-                                        <Card.Title>{event.event_name}</Card.Title>
-                                        <Card.Text>
-                                            {event.event_description}
-                                            <br />
-                                            <strong>Inicio:</strong> {new Date(event.event_start_date).toLocaleString()}
-                                            <br />
-                                            <strong>Fin:</strong> {new Date(event.event_end_date).toLocaleString()}
-                                        </Card.Text>
+        <>
+            <Card className='eventCard'>
+                <Card.Body>
+                    <Card.Title className='eventCard__title'>Mis Eventos</Card.Title>
+                    <Card.Text className='eventCard__text'>
+                        Aquí puedes ver todos tus eventos programados.
+                    </Card.Text>
+                    <ul className='eventCard__list'>
+                        {Array.isArray(events) && events.length > 0 ? (
+                            events.map((event) => (
+                                <li key={event.id_events} className='mb-3' style={{ listStyle: 'none' }}>
+                                    <Card>
+                                        <Card.Body className='eventCard__body'> 
+                                            <Card.Title>{event.event_name}</Card.Title>
+                                            <Card.Text>
+                                                {event.event_description}
+                                                <br />
+                                                <strong>Inicio:</strong> {new Date(event.event_start_date).toLocaleString()}
+                                                <br />
+                                                <strong>Fin:</strong> {new Date(event.event_end_date).toLocaleString()}
+                                            </Card.Text>
+                                        </Card.Body>
+                                        <div className='eventCard__button'>
+                                            <Button variant="primary" className='eventCard__buttonUpdate' onClick={()=> handleEdit (event)}>Editar</Button>
+                                            <DeleteEventsComponent event={event} />
+                                        </div>
+                                    </Card>
+                                </li>
+                            ))
+                        ) : (
+                            <li style={{ listStyle: 'none' }}>
+                                <Card className="text-center text-muted">
+                                    <Card.Body>
+                                        <Card.Text>No hay eventos programados.</Card.Text>
                                     </Card.Body>
-                                    <div className='eventCard__button'>
-                                        <Button variant="primary" className='eventCard__buttonUpdate' onClick={()=> updateEvents(event.id_events)}>Editar</Button>
-                                        <Button variant="danger" className='eventCard__buttonDelete'>Elimar</Button>
-                                    </div>
                                 </Card>
                             </li>
-                        ))
-                    ) : (
-                        <li style={{ listStyle: 'none' }}>
-                            <Card className="text-center text-muted">
-                                <Card.Body>
-                                    <Card.Text>No hay eventos programados.</Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </li>
-                    )}
-                </ul>
-            </Card.Body>
-        </Card>
+                        )}
+                    </ul>
+                </Card.Body>
+            </Card>
+            <UpdateEventsComponent/>
+        </>
+        
+        
     );
 }
 
