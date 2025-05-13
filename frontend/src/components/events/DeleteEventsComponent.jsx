d51ea6eab3d1b0e008b5154e9768084569ddbf00
mrMcAko <alechacin08@gmail.com>
2025-05-13 14:14:14 -0600

    Creamos el componente que permite eliminar eventos.
    Ademas implementamos la logica detrás haciendo posible que este se elimine

---
+++ frontend/src/components/events/DeleteEventsComponent.jsx
@@ -0,0 +1,39 @@
+import { Button } from 'react-bootstrap';
+import { useEventContext } from '@/hooks/useEventContext';
+import { useAuthContext } from '@/hooks/useAuthContext';
+
+const API_URL = import.meta.env.VITE_API_URL; // URL de la API desde el archivo .env
+
+const DeleteEventsComponent = ({event}) => {
+    const { events, setEvents  } = useEventContext();
+    const { token } = useAuthContext();
+
+    const handleDelete = async () => {
+        if (!window.confirm('¿Estás seguro de que deseas eliminar este evento?'));
+
+        try {
+            const response = await fetch(`${API_URL}/taskly/events/${event.id_events}/hard`, {
+                method: 'DELETE',
+                headers:{
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                    }
+                });
+
+            if (!response.ok) {
+                throw new Error('Error al eliminar el evento');
+            }
+
+        setEvents(events.filter((e) => e.id_events !== event.id_events));
+
+        } catch (error) {
+            console.error('Error al eliminar el evento:', error);
+        }
+    }
+
+    return (
+        <Button variant="danger" className='eventCard__buttonDelete' onClick={handleDelete}>Elimar</Button>
+    );
+}
+
+export default DeleteEventsComponent;
