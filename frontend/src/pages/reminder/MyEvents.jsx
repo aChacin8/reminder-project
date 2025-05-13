066ab2ea798e1d96b4030357ec2e5ec014aee4b7
mrMcAko <alechacin08@gmail.com>
2025-05-13 01:06:12 -0600

    Agregamos la pagina que contiene los eventos a editar o eliminar

---
+++ frontend/src/pages/reminder/MyEvents.jsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header';
+import EventsComponent from '@/components/events/EventsComponent';
+const MyEvents = () => {
+
+    return (
+        <>
+            <Header/>
+            <EventsComponent/>      
+        </>
+    );
+}
+
+export default MyEvents;
