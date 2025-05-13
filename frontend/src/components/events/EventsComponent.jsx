6cdab9cc2e36e7357e0ba7ba9460292ff57f2637
mrMcAko <alechacin08@gmail.com>
2025-05-13 01:04:44 -0600

    Creamos el componente de para modificar o eliminar eventos

---
+++ frontend/src/components/events/EventsComponent.jsx
@@ -0,0 +1,96 @@
+import { Card, Button } from 'react-bootstrap';
+import { useEffect } from 'react';
+import { useEventContext } from '@/hooks/useEventContext';
+import { useAuthContext } from '@/hooks/useAuthContext';
+import '@/styles/Calendar.scss';
+
+const API_URL = import.meta.env.VITE_API_URL; // URL de la API desde el archivo .env
+
+const EventsComponent = () => {
+    const { events, setEvents } = useEventContext();
+    const { token } = useAuthContext();
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const response = await fetch(`${API_URL}/taskly/events`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                const result = await response.json();
+                setEvents(result);
+            } catch (error) {
+                console.error('Error al llamar a los eventos:', error);
+            }
+        };
+        fetchEvents();
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [setEvents]); // Se asegura de que el efecto se ejecute solo una vez al montar el componente  
+
+    const updateEvents = async (data) => {
+        try {
+                
+                const response = await fetch(`${API_URL}/taskly/events/${data.id_event}`, {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`
+                    },
+                    body: JSON.stringify(data)
+                });
+                const result = await response.json();
+                setEvents(result);
+            } catch (error) {
+                console.error('Error actualizar eventos:', error);
+            }
+        };
+
+    return (
+        <Card className='eventCard'>
+            <Card.Body>
+                <Card.Title className='eventCard__title'>Mis Eventos</Card.Title>
+                <Card.Text className='eventCard__text'>
+                    Aquí puedes ver todos tus eventos programados.
+                </Card.Text>
+                <ul className='eventCard__list'>
+                    {Array.isArray(events) && events.length > 0 ? (
+                        events.map((event) => (
+                            <li key={event.id_events} className='mb-3' style={{ listStyle: 'none' }}>
+                                <Card>
+                                    <Card.Body className='eventCard__body'> 
+                                        <Card.Title>{event.event_name}</Card.Title>
+                                        <Card.Text>
+                                            {event.event_description}
+                                            <br />
+                                            <strong>Inicio:</strong> {new Date(event.event_start_date).toLocaleString()}
+                                            <br />
+                                            <strong>Fin:</strong> {new Date(event.event_end_date).toLocaleString()}
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <div className='eventCard__button'>
+                                        <Button variant="primary" className='eventCard__buttonUpdate' onClick={()=> updateEvents(event.id_events)}>Editar</Button>
+                                        <Button variant="danger" className='eventCard__buttonDelete'>Elimar</Button>
+                                    </div>
+                                </Card>
+                            </li>
+                        ))
+                    ) : (
+                        <li style={{ listStyle: 'none' }}>
+                            <Card className="text-center text-muted">
+                                <Card.Body>
+                                    <Card.Text>No hay eventos programados.</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </li>
+                    )}
+                </ul>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default EventsComponent;
