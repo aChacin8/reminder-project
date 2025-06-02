68572159537d4dcea753c8f328a5c5008c00456a
Iván Almaraz <ivanalmaraz@MacBook-Air-de-Ivan.local>
2025-06-02 12:03:52 -0600

    modificacionesFrontendIvan

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// src/components/Footer.jsx
+
+import React from 'react';
+import '@/styles/Footer.scss'; // Asegúrate de que la ruta esté bien desde donde lo llames
+
+const Footer = () => {
+  return (
+    <footer className="sticky-footer">
+      <p>© {new Date().getFullYear()} Taskly - Todos los derechos reservados</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,15 +1,19 @@
 import InfoComponent from '@/components/InfoComponent';
 import '@/styles/Home.scss';
 import Header from '@/components/Header';
+import Footer from '@/components/Footer'; // <- Importamos el Footer
 
 function Home () {
   return (
-        <div className='home'>
-            <Header/>
-            <div className='home__components'>
-              <InfoComponent/>
-            </div>
-        </div>
+    <div className='home'>
+      <Header />
+      
+      <div className='home__components'>
+        <InfoComponent />
+      </div>
+
+      <Footer /> {/* <- Insertamos el Footer aquí */}
+    </div>
   );
 }
 
