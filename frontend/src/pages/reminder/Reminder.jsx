651853d1ea445698f6c6dc0ba10babcfba4a1fe3
Iván Almaraz <ivanalmaraz@MacBook-Air-de-Ivan.local>
2025-06-02 18:25:34 -0600

    BackgroundReminder

--- frontend/src/pages/reminder/Reminder.jsx
+++ frontend/src/pages/reminder/Reminder.jsx
@@ -1,19 +1,18 @@
 import CalendarComponent from "@/components/calendar/CalendarComponent";
 import Header from '@/components/Header';
 import CardCalendarComponent from "@/components/calendar/CardCalendarComponent";
+import '@/styles/reminder.scss';
 
 const Reminder = () => {
-
     return (
-        <>
-            <Header/>
-            <div className="d-flex">
-                <CardCalendarComponent/>
-                <CalendarComponent/>
+        <div className="reminder"> {/* ← Esta clase aplica los estilos del fondo */}
+            <Header />
+            <div className="d-flex reminder__components">
+                <CardCalendarComponent />
+                <CalendarComponent />
             </div>
-            
-        </>
+        </div>
     );
-}
+};
 
 export default Reminder;
