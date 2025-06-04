71697148eef3f2ba52eca030801f776c59aa054b
Iván Almaraz <ivanalmaraz@MacBook-Air-de-Ivan.local>
2025-06-03 18:30:05 -0600

    AddCarousel

---
+++ frontend/src/components/HomeCarousel.jsx
@@ -0,0 +1,30 @@
+// components/home/HomeCarousel.jsx
+import { Carousel } from 'react-bootstrap';
+import img1 from '@/img/carrusel1.jpeg';
+import img2 from '@/img/carrusel2.jpg';
+import '@/styles/HomeCarousel.scss'
+const HomeCarousel = () => {
+  return (
+    <div className = 'home__carousel'>
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src={img1} alt="Primera imagen" />
+        <Carousel.Caption>
+          <h3>Bienvenido a Taskly</h3>
+          <p>Organiza tus tareas como nunca antes.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img className="d-block w-100" src={img2} alt="Segunda imagen" />
+        <Carousel.Caption>
+          <h3>Personaliza tu calendario</h3>
+          <p>Visualiza tus eventos de forma sencilla.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+  );
+};
+
+export default HomeCarousel;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import InfoComponent from '@/components/InfoComponent';
 import '@/styles/Home.scss';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer'; // <- Importamos el Footer
+import HomeCarousel from '@/components/HomeCarousel';
+
 
 function Home () {
   return (
     <div className='home'>
       <Header />
-      
+      <HomeCarousel />
       <div className='home__components'>
         <InfoComponent />
       </div>
