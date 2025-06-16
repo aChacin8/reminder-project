95da9e917aedf42a1b0d3737c044b6514736e621
aChacin8 <alechacin08@gmail.com>
2025-06-16 12:14:10 -0600

    fix: Actualizamos estilos para registrar el usuario.

--- frontend/src/pages/auth/SignUp.jsx
+++ frontend/src/pages/auth/SignUp.jsx
@@ -4,31 +4,31 @@ import { useForm } from 'react-hook-form';
 import {useNavigate} from 'react-router-dom';
 import '@/styles/Auth.scss'
 
-const API_URL = import.meta.env.VITE_API_URL; // URL de la API desde el archivo .env
+const API_URL = import.meta.env.VITE_API_URL; 
 
 const SingUp = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm(); // Form hook para manejar el formulario
-    const navigate = useNavigate(); // Hook para navegar entre rutas
+    const { register, handleSubmit, formState: { errors } } = useForm(); 
+    const navigate = useNavigate(); 
 
     const onSubmit = async(data) => {
         try {
-            console.log('backendURL:', API_URL); // <-- debería decir "http://localhost:3000"
+            console.log('backendURL:', API_URL); 
 
             const response = await fetch (`${API_URL}/taskly/register`, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json' // Indica que el cuerpo de la solicitud es JSON
+                    'Content-Type': 'application/json' 
                 },
-                body: JSON.stringify(data) // Convierte el objeto "data" a formato JSON
+                body: JSON.stringify(data) 
             });
             
-            const result = await response.json(); // Convierte la respuesta a formato JSON
+            const result = await response.json(); 
             
             if (response.status === 201) { 
-                alert('Usuario registrado con éxito'); // Muestra un mensaje de éxito                
-                navigate('/Login'); // Redirige al usuario a la página de inicio de sesión
+                alert('Usuario registrado con éxito');              
+                navigate('/Login'); 
             }
-            console.log(result); // Muestra el resultado en la consola
+            console.log(result);
             
         } catch (error) {
             console.error(error);
@@ -39,11 +39,11 @@ const SingUp = () => {
         <>
         <div className='signup'>
         <Header/>
-        <Card style={{ width: '25rem' }} className='justify-content-center mx-auto mt-5' id='signup'>
+        <Card style={{ width: '25rem', height: '47rem'}} className='justify-content-center mx-auto mt-4' id='signup'>
             <Card.Body className='text-center' id='signup__body'>
                 <Card.Title id='signup__tittle'>Crear Usuario</Card.Title>
                 <Form onSubmit={handleSubmit(onSubmit)} >
-                    <Form.Group className='mt-3' id='signup__form-div'>
+                    <Form.Group className='mt-2' id='signup__form-div'>
                         <Form.Group className='mb-2' id='signup__form-name'>
                             <Form.Label className='m-3'>Nombre:</Form.Label>
                             <Form.Control
@@ -73,10 +73,10 @@ const SingUp = () => {
                         </Form.Group>
                     </Form.Group>
 
-                    <Form.Group className="mb-3 d-flex">
-                        <Form.Label className="mx-4">Género:</Form.Label>
+                    <Form.Group className="d-flex">
+                        <Form.Label className="m-2">Género:</Form.Label>
                         {['H', 'M', 'O'].map((value) => (
-                            <div key={value} className="mb-3">
+                            <div key={value} className="mt-2 mb-3">
                                 <Form.Check
                                     inline
                                     label={value === 'H' ? 'Hombre' : value === 'M' ? 'Mujer' : 'Otro'}
@@ -91,13 +91,13 @@ const SingUp = () => {
                         <p>{errors.gender?.message}</p>
                     </Form.Group>
 
-                    <Form.Group className='mt-3' id='signup__form-div'>
+                    <Form.Group className='mt-2' id='signup__form-div'>
                         <Form.Group className='mb-3' id='signup__form-address'>
                             <Form.Label className='m-3'>Direccion:</Form.Label>
                             <Form.Control
                                 type='text'
                                 id='signup__address'
-                                className='mt-3'
+                                className='mt-2'
                                 name='address'
                                 placeholder='Ingresa tu direccion'
                                 required
@@ -110,7 +110,7 @@ const SingUp = () => {
                             <Form.Control
                                 type='text'
                                 id='signup__phone'
-                                className='m-3'
+                                className='m-2'
                                 name='phone_num'
                                 pattern="^\d{10}$"
                                 placeholder='Ingresa tu numero telefonico'
@@ -132,7 +132,7 @@ const SingUp = () => {
                         <Form.Control
                             type='email'
                             name='email'
-                            className='m-3'
+                            className='m-2'
                             id='signup__email'
                             placeholder='nombre@ejemplo.com'
                             required
@@ -170,7 +170,7 @@ const SingUp = () => {
                     <Button 
                         variant='success' 
                         type='submit' 
-                        className='btn btn-outline-primary mt-3' 
+                        className='btn btn-outline-primary mb-5' 
                         id='signup__btn'
                         >
                             Registrarse 
